fbbfc858c92a686c763d10fc4b6f913f197b98e8
Tathyanna Maximiano <tathyanna.maximiano@gmail.com>
2022-10-16 01:57:00 -0400

    event size done

--- src/components/Mediators/index.js
+++ src/components/Mediators/index.js
@@ -1,17 +1,46 @@
 import './styles.modules.css';
+import IvanLogo from '../../assets/ivan.jpeg';
+import BrunoLogo from '../../assets/bruno.jpeg';
 
 export function Mediators() {
 	return (
 		<>
+			<div className="faixaDiv">
+				<h2>Anfitriões</h2>
+			</div>
 			<div className="moderDiv">
-				<img src="../../assets/ivan.jpeg" alt="logo ivan"></img>
-				<h3>Ivan Salles</h3>
-				<p>
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis
-					sunt quidem aliquam cupiditate. Quaerat quidem omnis, asperiores quia,
-					optio recusandae amet, laboriosam nostrum alias nobis similique
-					expedita voluptates odio minima
-				</p>
+				<div className="moderInfoDiv">
+					<img src={BrunoLogo} alt="logo ivan"></img>
+					<h4>Bruno Guerreiro</h4>
+					<p>
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis
+						sunt quidem aliquam cupiditate.
+					</p>
+				</div>
+				<div className="moderInfoDiv">
+					<img src={IvanLogo} alt="logo ivan"></img>
+					<h4>Ivan Salles</h4>
+					<p>
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis
+						sunt quidem aliquam cupiditate.
+					</p>
+				</div>
+				<div className="moderInfoDiv">
+					<img src="../../assets/ivan.jpeg" alt="logo ivan"></img>
+					<h4>Luccas Bertti</h4>
+					<p>
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis
+						sunt quidem aliquam cupiditate.
+					</p>
+				</div>
+				<div className="moderInfoDiv">
+					<img src="../../assets/ivan.jpeg" alt="logo ivan"></img>
+					<h4>Kramer Saunders</h4>
+					<p>
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis
+						sunt quidem aliquam cupiditate.
+					</p>
+				</div>
 			</div>
 		</>
 	);
