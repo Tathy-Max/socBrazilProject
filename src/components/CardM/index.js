cdb40d26cb15319e986804830544bf89a7b59ff2
Tathyanna Maximiano <tathyanna.maximiano@gmail.com>
2022-11-03 18:55:55 -0400

    testing info div

--- src/components/CardM/index.js
+++ src/components/CardM/index.js
@@ -6,11 +6,15 @@ export function CardM({ speakers }) {
 	return (
 		<div className={style.cardSDiv}>
 			<h2>{speakers.day}</h2>
-			<img src={`${speakers.imageS}`} alt="speakers logo" />
+			<img
+				className={style.imgSpeaker}
+				src={`${speakers.imageS}`}
+				alt="speakers logo"
+			/>
 			<h3>{speakers.name}</h3>
 			<p className={style.pCardT}>{speakers.topicS}</p>
 			<a href={speakers.link} target="_blank" rel="noreferrer">
-				<img id={style.imgC} src={speakers.imageC} alt="speakers logo" />
+				<img className={style.imgC} src={speakers.imageC} alt="speakers logo" />
 			</a>
 			<p>{speakers.topicC}</p>
 		</div>
